aab6b25c33f81260e9e7402f37597972aeda1a8d
Süleyman Talha Akdoğan <suleymantalhaakdogan@gmail.com>
2025-03-23 18:16:21 +0300

    explanation of errors HW1-Q2.c

--- HW1-Q2.c
+++ HW1-Q2.c
@@ -1,23 +1,23 @@
 /* Addition program */
 #include <stdio.h>
 
-/* function main begins program execution */
+/* THE ERRORS ARE EXPLAINED WITH COMMENTS */
 int main( void )
 {
-   int integer1; /* first number to be input by user  */
-   int integer2; /* second number to be input by user */
-   int x;      /* variable in which sum will be stored */ 
+   int integer1; /*"The semicolon is missing."*/
+   int integer2;  /*"The semicolon is missing."*/
+   int x;      /* An unnecessary variable defining due to never being used in any way. */ 
 
-   printf( "Enter first integer\n" ); /* prompt */
-   scanf( "%d", &integer1 );        /* read an integer */
+   printf( "Enter first integer\n" ); /* The quotation mark is missing. */
+   scanf( "%d", &integer1 );        
 
-   printf( "Enter second integer\n" ); /* prompt */
-   scanf( "%d", &integer2 );           /* read an integer */
+   printf( "Enter second integer\n" ); 
+   scanf( "%d", &integer2 );           
    
-   int sum = integer1 + integer2; /* assign total to sum */
+   int sum = integer1 + integer2; /* The variable type is not specified. */
 
-   printf( "Sum is %d\n", sum ); /* print sum */
-   return 0; /* indicate that program ended successfully */
+   printf( "Sum is %d\n", sum ); 
+   return 0; 
 
 } /* end function main */
 
