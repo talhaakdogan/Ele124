e6c0f1e44525bfcb9dd9f83fef6cbbf56f1c2647
Süleyman Talha Akdoğan <suleymantalhaakdogan@gmail.com>
2025-05-08 21:55:35 +0300

    Create Q3.c

---
+++ HW2/Q3.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void Hunerler ( float itemPrice, float* donationTotal , float* profitTotal);
+
+int main(){
+
+    float donation=0;
+    float profit=0;
+    float itemPrice;
+
+    FILE *file_donation=fopen("donations.txt", "w");
+
+
+    //codes provide random numbers from previous exercise.
+    srand(time(NULL));
+
+    float aRandomNumber ;
+
+    for(int i=0; i<50; i++){
+        aRandomNumber = rand()%2000 + 1; //Generates a random number between 20000 and 1.
+        aRandomNumber = aRandomNumber/100; //Converts it to a float number between 20 and 0.
+        itemPrice=aRandomNumber;
+        Hunerler(itemPrice, &donation, &profit);
+        fprintf(file_donation ,"Item: %.2f\n", itemPrice);
+
+    }
+
+    fprintf(file_donation, "Donation: %.2f\n", donation);
+    fprintf(file_donation,"Profit: %.f\n", profit);
+
+    fclose(file_donation);
+
+
+    return 0;
+
+}
+
+void Hunerler( float itemPrice, float* donationTotal, float* profitTotal){
+
+
+    float frac= itemPrice - (int)itemPrice;
+    *donationTotal = *donationTotal + frac;
+    *profitTotal = *profitTotal + (int)itemPrice;
+
+
+
+}
