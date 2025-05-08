5f12d2f013a4d9a53b0b7e32b79df96f517259d7
Süleyman Talha Akdoğan <suleymantalhaakdogan@gmail.com>
2025-05-08 13:34:33 +0300

    Create Q2.c

---
+++ HW2/Q2.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void Hunerler ( float itemPrice, float* donationTotal , float* profitTotal);
+
+int main(){
+    
+    float donation=0;
+    float profit=0;
+    float itemPrice;
+    //random codes of previous exercise.
+    srand(time(NULL));
+
+    float aRandomNumber ; 
+    
+    for(int i=0; i<50; i++){
+        aRandomNumber = rand()%20000 + 1; //Generates a random number between 20000 and 1.
+        aRandomNumber = aRandomNumber/1000; //Converts it to a float number between 20 and 0.
+        
+        itemPrice=aRandomNumber;
+        Hunerler(itemPrice, &donation, &profit);
+        printf("%.2f\n", itemPrice);
+        
+    }
+    
+    printf("Donation: %.2f\n", donation);
+    printf("Profit: %.f\n", profit);
+    
+
+    return 0;
+        
+}
+
+void Hunerler( float itemPrice, float* donationTotal, float* profitTotal){
+    
+    
+    float frac= itemPrice - (int)itemPrice;
+    *donationTotal = *donationTotal + frac;
+    *profitTotal = *profitTotal + (int)itemPrice;
+    
+    
+    
+}
